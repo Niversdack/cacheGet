a2f2178ce17fdb1e035abbc021fe55057d3f99ec
Niversdack <bb6bbe@gmail.com>
2021-05-19 22:06:47 +0500

    Add Rw mutex in cache

--- cache/cache.go
+++ cache/cache.go
@@ -8,28 +8,27 @@ import (
 
 type Cache struct {
 	sync.RWMutex
-	items             map[string]Item
+	items    map[string]Item
 	maxValue uint64
-
 }
 
 type Item struct {
-	Value      []byte
-	CreateAt   time.Time
+	Value    []byte
+	CreateAt time.Time
 }
 
 func New(maxValue uint64) *Cache {
-	items:=make(map[string]Item)
-	cache:=Cache{
+	items := make(map[string]Item)
+	cache := Cache{
 		items:    items,
 		maxValue: maxValue,
 	}
 	return &cache
 }
 
-func (c *Cache) Set(key string, value  []byte) {
+func (c *Cache) Set(key string, value []byte) {
 
-	if int(c.maxValue)==len(c.items){
+	if int(c.maxValue) == len(c.items) {
 		c.del()
 	}
 	c.Lock()
@@ -37,15 +36,15 @@ func (c *Cache) Set(key string, value  []byte) {
 	defer c.Unlock()
 
 	c.items[key] = Item{
-		Value:      value,
-		CreateAt:    time.Now(),
+		Value:    value,
+		CreateAt: time.Now(),
 	}
 
 }
 
 type reviewsData struct {
-	key      string
-	time   time.Time
+	key  string
+	time time.Time
 }
 
 type timeSlice []reviewsData
@@ -62,12 +61,15 @@ func (p timeSlice) swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
 
-func (c *Cache) sortByTime()([]reviewsData){
+func (c *Cache) sortByTime() []reviewsData {
+	c.RLock()
+
+	defer c.RUnlock()
 	result := make(timeSlice, 0, len(c.items))
 	for key, d := range c.items {
-		result = append(result,reviewsData{
-			key: key,
-			time:  d.CreateAt,
+		result = append(result, reviewsData{
+			key:  key,
+			time: d.CreateAt,
 		})
 	}
 	sort.Slice(result, func(i, j int) bool {
@@ -75,9 +77,12 @@ func (c *Cache) sortByTime()([]reviewsData){
 	})
 	return result
 }
-func (c *Cache) del(){
-	sortItems:=c.sortByTime()
-	delete(c.items,sortItems[0].key)
+func (c *Cache) del() {
+	c.RLock()
+
+	defer c.RUnlock()
+	sortItems := c.sortByTime()
+	delete(c.items, sortItems[0].key)
 }
 func (c *Cache) Get(key string) (*[]byte, bool) {
 
